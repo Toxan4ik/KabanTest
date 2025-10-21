fcedcc25716caf514a3101ce09e9858275001b3f
Toxan4ik <108747572+Toxan4ik@users.noreply.github.com>
2025-10-21 19:56:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from openai import AsyncClient
+
+from bs4 import BeautifulSoup
+import requests
+
+import random
+
+bot = Bot(token="7783613623:AAHHSWVi7HlB6PhX2rEmJeFNCZKMXO7UJGI") # кабан 7746997930:AAGisN5ApKcZV53BbDXya2an0Jn9OKCJOFE   погода 7783613623:AAHHSWVi7HlB6PhX2rEmJeFNCZKMXO7UJGI
+dp = Dispatcher()
+
+@dp.message(Command("casino"))
+async def casino_command(message: types.Message):
+	casino_message = await message.reply_dice(emoji="🎰")
+	await asyncio.sleep(1.5)
+	await message.reply(text="Ваш выигрыш "+message.from_user.first_name)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
